a935f740726fe93c9a6d6747acad946cff4b0cab
Abraham Nieva <nievadelahidalgaa@cardiff.ac.uk>
2019-08-19 14:32:35 +0100

    cleaunp after deploying into VM 2

--- testing/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# install ansible latest version
-sudo apt update
-sudo apt install software-properties-common
-sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
